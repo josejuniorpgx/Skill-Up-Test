a911afdfc16d65d37f2cb2ecea0f510b950c9ea5
josejuniorpgx <98073366+josejuniorpgx@users.noreply.github.com>
2025-06-10 18:09:24 -0600

    Refactor project structure: move files to `src` directory and update paths.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import '@mantine/core/styles.css';
 
 import React from 'react';
 import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
-import { theme } from '../theme';
+import { theme } from '../../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'Skill UP',
+  description: 'Hi Welcome to Skill UP!',
 };
 
 export default function RootLayout({ children }: { children: any }) {
